eff91a6700efb796b3c8a3520ffa9e8adf766f68
IvanBurk <64144110+IvanBurk@users.noreply.github.com>
2021-09-29 19:34:16 +0300

    push volume

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -4,14 +4,24 @@ public class Radio {
     private int maxStation = 9;
     private int minStation = 0;
     private int currentStation;
+    private int increaseVolume;
+    private int minVolume = 0;
+    private int maxVolume = 10;
 
-    public void setCurrentStation(int currentStation) {
-        if (currentStation <= minStation) {
-            currentStation = maxStation;
-        } else if (currentStation >= maxStation) {
+    public void setCurrentStationNext(int currentStation) {
+        if (currentStation >= maxStation) {
             currentStation = minStation;
         } else {
             currentStation = currentStation + 1;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void setCurrentStationPrew(int currentStation) {
+        if (currentStation <= minStation) {
+            currentStation = maxStation;
+        } else {
+            currentStation = currentStation - 1;
         }
         this.currentStation = currentStation;
     }
@@ -22,5 +32,27 @@ public class Radio {
 
     public void setCurrentStationValid(int currentStation) {
         this.currentStation = currentStation;
+    }
+
+    public void setIncreaseVolumeUp(int increaseVolume) {
+        if (increaseVolume > maxVolume) {
+            this.increaseVolume = maxVolume;
+            return;
+        } else {
+            this.increaseVolume = increaseVolume + 1;
+        }
+    }
+
+    public void setIncreaseVolumeDown(int increaseVolume) {
+        if (increaseVolume < minVolume) {
+            this.increaseVolume = minVolume;
+            return;
+        } else {
+            this.increaseVolume = increaseVolume - 1;
+        }
+    }
+
+    public int getIncreaseVolume() {
+       return increaseVolume;
     }
 }
--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -8,7 +8,7 @@ public class RadioTest {
     @Test
     void currentStationNext() {
         Radio dance = new Radio();
-        dance.setCurrentStation(9);
+        dance.setCurrentStationNext(9);
         int actual = dance.getCurrentStation();
 
         assertEquals(0, actual);
@@ -18,20 +18,38 @@ public class RadioTest {
     @Test
     void currentStationPrew() {
         Radio dance = new Radio();
-        dance.setCurrentStation(0);
+        dance.setCurrentStationNext(0);
         int actual = dance.getCurrentStation();
 
-        assertEquals(9, actual);
+        assertEquals(1, actual);
     }
 
 
     @Test
     void currentStation() {
         Radio dance = new Radio();
-        dance.setCurrentStation(5);
+        dance.setCurrentStationNext(5);
         int actual = dance.getCurrentStation();
 
         assertEquals(6, actual);
+    }
+
+    @Test
+    void currentStationReverse() {
+        Radio dance = new Radio();
+        dance.setCurrentStationPrew(9);
+        int actual = dance.getCurrentStation();
+
+        assertEquals(8, actual);
+    }
+
+    @Test
+    void currentStationReversePrew() {
+        Radio dance = new Radio();
+        dance.setCurrentStationPrew(0);
+        int actual = dance.getCurrentStation();
+
+        assertEquals(9, actual);
     }
 
     @Test
@@ -41,5 +59,40 @@ public class RadioTest {
         int actual = dance.getCurrentStation();
 
         assertEquals(5, actual);
+    }
+
+    @Test
+    void increaseVolume() {
+        Radio dance_v = new Radio();
+        dance_v.setIncreaseVolumeUp(11);
+        int actual = dance_v.getIncreaseVolume();
+
+        assertEquals(10, actual);
+    }
+
+    @Test
+    void increaseVolumeNext() {
+        Radio dance_v = new Radio();
+        dance_v.setIncreaseVolumeUp(6);
+        int actual = dance_v.getIncreaseVolume();
+
+        assertEquals(7, actual);
+    }
+
+    @Test
+    void increaseVolumePrew() {
+        Radio dance_v = new Radio();
+        dance_v.setIncreaseVolumeDown(6);
+        int actual = dance_v.getIncreaseVolume();
+
+        assertEquals(5, actual);
+    }
+
+    @Test
+    void increaseVolumePrev() {
+        Radio dance_v = new Radio();
+        dance_v.setIncreaseVolumeDown(-1);
+        int actual = dance_v.getIncreaseVolume();
+        assertEquals(0, actual);
     }
 }
